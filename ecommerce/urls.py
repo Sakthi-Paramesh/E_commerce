bcd6f03cda4f591f17bd5326fede81d94f03b93a
Sakthi Paramesh <sakthiparameshbala@gmail.com>
2026-09-10 22:37:46 +0530

    Fix media image serving

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('shop.urls')),
     path('accounts/', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+# Serve uploaded media files
+urlpatterns += [
+    path(
+        'media/<path:path>',
+        serve,
+        {'document_root': settings.MEDIA_ROOT},
+    ),
+]
